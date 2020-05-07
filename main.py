bb748e853197918b1229e0170dc05e632ff3963e
Irae Hueck Costa <mail@irae.me>
2020-05-07 15:14:36 +0200

    -

--- main.py
+++ main.py
@@ -8,20 +8,29 @@ import datetime
 from urllib.parse import urlparse
 import hashlib
 
-from ua_parser import user_agent_parser
+from device_detector import DeviceDetector
 
 
 r = redis.StrictRedis()
 
 
-
+STORE_AS_HASH = ["os", "dev", "browser", "date"]
 MAXSIZE = 64
 MAX_ENTRIES = 20
+CHOICES = {
+    "os": ["android", "ios", "windows", "linux"],
+    "dev": ["desktop", "smartphone", "tablet"],
+    "browser": ["internet explorer", "firefox", "chrome", "safari"],
+}
 
 
-@app.route('/unique/<uid>/')
-def unique(uid):
+def to_choice(key, value):
+    print(key, value)
+    if value.lower() in CHOICES[key]:
+        return value
+    return "Other"
 
+def get_current_date(request):
     utcoffset = request.args.get('utcoffset', '')
     try:
         utcoffset = int(utcoffset)
@@ -34,32 +43,59 @@ def unique(uid):
 
     now = datetime.datetime.utcnow()
     now += datetime.timedelta(hours=utcoffset)
+    date = str(now.date())
+    return date
+
+
+def get_insights(request):
+    insights = {}
+
+    insights["date"] = get_current_date(request)
+
+    ua = request.headers.get('User-Agent')
+    if ua:
+        pua = DeviceDetector(ua).parse()
+        insights["browser"] = to_choice("browser", pua.client_name())
+        insights["dev"] = to_choice("dev", pua.device_type())
+        insights["os"] = to_choice("os", pua.os_name())
+
+    if request.accept_languages:
+        al = sorted(request.accept_languages, key=lambda x: x[1], reverse=True)[0][0]
+        al = al.split('-')[0]
+        insights["lang"] = al
+
+    referrer = request.args.get("referrer") # passed with javascript!!
+    if referrer:
+        parsed = urlparse(request.referrer)
+        insights["refd"] = parsed.netloc
+        insights["refp"] = parsed.path
+
+    return insights
+
+
+
+
+@app.route('/unique/<uid>/')
+def unique(uid):
+    
+    insights = get_insights(request)
 
     with r.pipeline() as pipe:
-        referrer = request.args.get("referrer") # passed with javascript!!
-        if referrer:
-            parsed = urlparse(request.referrer)
-            pipe.zincrby(f"referrer:{uid}", 1, parsed.netloc[:MAXSIZE])
-        else:
-            pipe.zincrby(f"referrer:{uid}", 1, "Direct")
 
-        ua = request.headers.get('User-Agent')
-        if ua:
-            pua = user_agent_parser.Parse(ua)
-            os = pua['os']['family']
-            browser = pua['user_agent']['family']
-            pipe.zincrby(f"os:{uid}", 1, os[:MAXSIZE])
-            pipe.zincrby(f"browser:{uid}", 1, browser[:MAXSIZE])
+        for key, value in insights.items():
+            if key in STORE_AS_HASH:
+                pipe.hincrby(f"{key}:{uid}", value, 1)
+            else:
+                pipe.zincrby(f"{key}:{uid}", 1, value)
 
-        # every 100s request with random
-        pipe.zremrangebyrank(f"referrer:{uid}", 0, -1 * MAX_ENTRIES)
-        pipe.zremrangebyrank(f"os:{uid}", 0, -1 * MAX_ENTRIES)
-        pipe.zremrangebyrank(f"browser:{uid}", 0, -1 * MAX_ENTRIES)
+        ## every 100s request with random
+        #pipe.zremrangebyrank(f"referrer:{uid}", 0, -1 * MAX_ENTRIES)
+        #pipe.zremrangebyrank(f"os:{uid}", 0, -1 * MAX_ENTRIES)
+        #pipe.zremrangebyrank(f"browser:{uid}", 0, -1 * MAX_ENTRIES)
 
-        # every 10s request iwth random
-        #refresh_keys(username)
+        ## every 10s request iwth random
+        ##refresh_keys(username)
 
-        pipe.hincrby(f"days:{uid}", str(now.date()), 1)
         pipe.execute()
 
     return ''
@@ -104,14 +140,17 @@ def index():
         pipe.zrange(f"referrer:{username}", 0, 10, withscores=True)
         pipe.zrange(f"os:{username}", 0, 10, withscores=True)
         pipe.zrange(f"browser:{username}", 0, 10, withscores=True)
+        pipe.zrange(f"lang:{username}", 0, 10, withscores=True)
+        pipe.zrange(f"dev:{username}", 0, 10, withscores=True)
         pipe.hgetall(f"days:{username}")
         vals = pipe.execute()
-        referrer_zet, os_zet, browser_zet, days_hash = vals
+        referrer_zet, os_zet, browser_zet, lang_zet, dev_zet, days_hash = vals
 
-    fst_day = min(days_hash.keys())
-    day = datetime.datetime.strptime(fst_day.decode(), "%Y-%m-%d")
-    day_before = str((day - datetime.timedelta(days=1)).date())
-    days_hash[day_before.encode()] = 0
+    if days_hash:
+        fst_day = min(days_hash.keys())
+        day = datetime.datetime.strptime(fst_day.decode(), "%Y-%m-%d")
+        day_before = str((day - datetime.timedelta(days=1)).date())
+        days_hash[day_before.encode()] = 0
 
     days_hash = dict(sorted(days_hash.items()))
 
@@ -125,6 +164,12 @@ def index():
       browser_labels=[i.decode() for (i, _) in browser_zet],
       browser_values=[i for (_, i) in browser_zet],
 
+      lang_labels=[i.decode() for (i, _) in lang_zet],
+      lang_values=[i for (_, i) in lang_zet],
+
+      dev_labels=[i.decode() for (i, _) in dev_zet],
+      dev_values=[i for (_, i) in dev_zet],
+
       days_labels=[k for (k, v) in days_hash.items()],
       days_values=[v for (k, v) in days_hash.items()],
     
@@ -135,4 +180,6 @@ def index():
                 **templ_args)
 
 
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config["TESTING"] = True
 app.run()
