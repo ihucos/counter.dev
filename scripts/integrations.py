564b6799a228f965f0934dc4584d567929095737
Irae Hueck Costa <mail@irae.me>
2020-06-24 10:37:17 +0200

    -

---
+++ scripts/integrations.py
@@ -0,0 +1,31 @@
+import redis
+from datetime import datetime, timedelta
+
+r = redis.Redis()
+access = r.hgetall("access")
+dates_around_today = [
+    datetime.strftime(datetime.now() - timedelta(i), '%Y-%m-%d')
+    for i in [-1, 0, 1, 2, 3]
+]
+stats = []
+for key in r.keys("date:*"):
+    user = key.decode().split(":", 1)[-1]
+    date_data = r.hgetall(key)
+    hits = sum(int(i) for i in date_data.values())
+    sorted_dates = list(sorted(date_data.keys()))
+    origins = r.zrange("origin:{}".format(user), 0, -1)
+    last_tracked = sorted_dates[-1].decode()
+    active = last_tracked in dates_around_today
+    stats.append((
+        user,
+        "ok" if active else "",
+        sorted_dates[0].decode(),
+        access.get(user.encode(), b"0000-00-00").decode(),
+        hits,
+        b" ".join(origins).decode()))
+
+stats.sort(key=lambda i: i[2])
+
+print("user                 active integrated login      hits     sites")
+for line in stats:
+    print("{:<24} {:<2} {} {} {:<8,} {}".format(*line))
