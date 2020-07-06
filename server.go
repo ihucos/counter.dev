2d32a22dadf4e29889efb5013e1a1eae6a2e5105
Irae Hueck Costa <mail@irae.me>
2020-07-06 11:17:19 +0200

    -

--- server.go
+++ server.go
@@ -239,7 +239,6 @@ func Track(w http.ResponseWriter, r *http.Request) {
 	//
 	logLine := fmt.Sprintf("[%s] %s %s %s", now.Format("2006-01-02 15:04:05"), country, refParam, userAgent)
 	save(user, data, logLine)
-        w.WriteHeader(http.StatusNoContent)
 }
 
 func Register(w http.ResponseWriter, r *http.Request) {
