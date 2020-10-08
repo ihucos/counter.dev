bb4fa7a7b9784f1b36e3f983bbe2505c96f9147f
Irae Hueck Costa <mail@irae.me>
2020-10-08 09:08:49 +0200

    -

--- static/comps/charts.js
+++ static/comps/charts.js
@@ -154,3 +154,104 @@ customElements.define('counter-weekday',
             })
         }
     })
+
+
+customElements.define('counter-top-referrers',
+    class extends HTMLElement {
+        constructor() {
+            super()
+        }
+
+        makeGradient(alpha1, alpha2) {
+            alpha1 = (typeof alpha1 !== 'undefined') ? alpha1 : 0.6;
+            alpha2 = (typeof alpha2 !== 'undefined') ? alpha2 : 1;
+            var ctx = this.canvas.getContext("2d")
+            var gradientStroke = ctx.createLinearGradient(0, 0, 0, 200);
+            gradientStroke.addColorStop(0, "rgba(30, 135, 240, " + alpha1 + ")");
+            gradientStroke.addColorStop(1, "rgba(30, 135, 240, " + alpha2 + ")");
+            return gradientStroke
+        }
+
+
+        set entries(refentries) {
+            this.innerHTML = "<canvas></canvas>"
+            this.canvas = this.children[0]
+
+            var colors = [palette[2], palette[1], palette[0]]
+            var otherColor = palette[3]
+            var directColor = 'rgba(0,0,0,0.12)'
+
+            var topRefs = dGroupData(refentries, 3)
+            var total = sum(Object.values(data.date)) ///////////// GLOBAL DATA REF
+            var ref = sum(Object.values(refentries))
+            var direct = total - ref
+            topRefs["Direct"] = direct
+
+            var entries = []
+            for (const [key, value] of Object.entries(topRefs)) {
+                if (key === "Direct") {
+                    var color = directColor
+                } else if (key === "Other") {
+                    var color = otherColor
+                } else {
+                    var color = colors.pop()
+                }
+                entries.push({
+                    label: key,
+                    value: value,
+                    color: color
+                })
+            }
+
+            new Chart(this.canvas, {
+                type: 'pie',
+                data: {
+                    labels: entries.map(x => x.label),
+                    datasets: [{
+                        borderWidth: pieBorderWidth,
+                        borderColor: pieBorderColor,
+                        data: emptyIfSumZero(entries.map(x => x.value)),
+                        backgroundColor: entries.map(x => x.color),
+                    }, ],
+                },
+                options: {
+                    //cutoutPercentage: 50,
+                    tooltips: {
+                        mode: 'index'
+                    },
+                    legend: {
+                        position: 'left',
+                        labels: {
+                            usePointStyle: 'true'
+                        },
+                        align: 'center'
+                    },
+                    title: {
+                        display: true,
+                        text: "Top Traffic Sources",
+                        position: "top",
+                    },
+                    scales: {
+                        xAxes: [{
+                            gridLines: {
+                                display: false,
+                            },
+                            ticks: {
+                                beginAtZero: true,
+                                display: false,
+                            }
+                        }, ],
+                        yAxes: [{
+                            gridLines: {
+                                display: false,
+                            },
+                            ticks: {
+                                beginAtZero: true,
+                                display: false,
+                            }
+                        }, ],
+                    },
+                },
+            })
+        }
+    })
--- static/draw.js
+++ static/draw.js
@@ -142,84 +142,6 @@ function drawPie(elemId, entries, title) {
 
 
 
-function drawRefChart(elemId) {
-    var colors = [palette[2], palette[1], palette[0]]
-    var otherColor = palette[3]
-    var directColor = 'rgba(0,0,0,0.12)'
-
-    var topRefs = dGroupData(data.ref, 3)
-    var total = sum(Object.values(data.date))
-    var ref = sum(Object.values(data.ref))
-    var direct = total - ref
-    topRefs["Direct"] = direct
-
-    var entries = []
-    for (const [key, value] of Object.entries(topRefs)) {
-        if (key === "Direct") {
-            var color = directColor
-        } else if (key === "Other") {
-            var color = otherColor
-        } else {
-            var color = colors.pop()
-        }
-        entries.push({
-            label: key,
-            value: value,
-            color: color
-        })
-    }
-
-    registerChart(new Chart(document.getElementById(elemId), {
-        type: 'pie',
-        data: {
-            labels: entries.map(x => x.label),
-            datasets: [{
-                borderWidth: pieBorderWidth,
-                borderColor: pieBorderColor,
-                data: emptyIfSumZero(entries.map(x => x.value)),
-                backgroundColor: entries.map(x => x.color),
-            }, ],
-        },
-        options: {
-            //cutoutPercentage: 50,
-            tooltips: {
-                mode: 'index'
-            },
-            legend: {
-                position: 'left',
-                labels: {
-                    usePointStyle: 'true'
-                },
-                align: 'center'
-            },
-            title: {
-                display: true,
-                text: "Top Traffic Sources",
-                position: "top",
-            },
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        display: false,
-                    },
-                    ticks: {
-                        beginAtZero: true,
-                        display: false,
-                    }
-                }, ],
-                yAxes: [{
-                    gridLines: {
-                        display: false,
-                    },
-                    ticks: {
-                        beginAtZero: true,
-                        display: false,
-                    }
-                }, ],
-            },
-        },
-    }))
-}
 
 
 function drawLastDays(elemId, date_keys, date_vals) {
@@ -302,8 +224,6 @@ function draw() {
     drawUTCOffsetVar()
     drawMap("world")
     drawTitle(user)
-    drawRefChart("ref_chart")
-
 
     var date_keys;
     var date_vals;
