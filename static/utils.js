eb16c3cdc996995e560281490f1fa1935d9e25ab
Irae Hueck Costa <mail@irae.me>
2020-10-11 17:19:45 +0200

    -

--- static/utils.js
+++ static/utils.js
@@ -1,4 +1,26 @@
 
+function setChartJSDefaults(){
+    // I don't completely get this one, but it is quite important
+    Chart.defaults.global.maintainAspectRatio = false
+    
+    Chart.defaults.global.title.fontFamily = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"';
+    Chart.defaults.global.title.fontColor = "rgba(0,0,0, 0.7)";
+    Chart.defaults.global.title.fontSize = 16
+    Chart.defaults.global.title.lineHeight = 1.2
+    Chart.defaults.global.title.padding = 10
+    Chart.defaults.global.layout = {
+        padding: {
+            left: 5,
+            right: 5,
+            top: 10,
+            bottom: 10
+        }
+    }
+}
+
+Chart.defaults.global.defaultFontFamily = '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"';
+
+
 function tagName() {
     var tagName = new URL(document.currentScript.src).pathname
         .slice(1, -3)
@@ -295,7 +317,10 @@ document.addEventListener('setup-user-auth', () => {
 })
 
 
+setChartJSDefaults()
 
 state = new StateMngr()
 setTimeout(()=> state.customElementsReady(), 500) // remove fake!
+
+
 
