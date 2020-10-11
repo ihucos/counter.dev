96f51ee504750c1b1bc0fdfa41c547333071fb58
Irae Hueck Costa <mail@irae.me>
2020-10-11 19:40:41 +0200

    -

--- static/comp/selector.js
+++ static/comp/selector.js
@@ -4,10 +4,12 @@ customElements.define(tagName(),
     class extends HTMLElement {
         draw(sites){
 
+                this.style.display = "flex"
+
+                // HTML INJECTION!!!!
                 this.innerHTML = `<form action="" class="flex" style="margin-left: auto;">
                    <select id="site-selector" onchange="onSiteChanged()" class="selector float-right shadow text-gray-800 bg-gray-400 text-sm font-bold py-2 pr-8 rounded inline-flex items-center appearance-none mr-1" name="time-range" style="color: rgba(0,0,0, 0.7); padding-left: 12px;">
-                      <option value="counter.dev">counter.dev</option>
-                      <option selected="selected" value="simple-web-analytics.com">simple-web-analytics.com</option>
+                      ${sites.map(site => `<option value="${site}">${site}</option>`).join('')}
                    </select>
                 </form>
                 
