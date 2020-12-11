2545d3149398edbcba7bc9b56e9bca68e709551b
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2020-12-11 20:16:29 +0100

    Selector componente working; only slight polish needed there

--- static/comp/newdesign/selector.js
+++ static/comp/newdesign/selector.js
@@ -5,51 +5,57 @@ customElements.define(
       this.style.display = "flex";
       this.style["margin-left"] = "5px";
 
-      this.innerHTML = `<form action="" class="flex" style="margin-left: auto;">
-                   <select onchange="onSiteChanged()" class="site-select selector float-right shadow text-gray-800 bg-gray-400 text-sm font-bold py-2 pr-8 rounded inline-flex items-center appearance-none mr-1" name="time-range" style="color: rgba(0,0,0, 0.7); padding-left: 12px;">
-                      ${sites
-                        .map(
-                          (site) =>
-                            `<option ${
-                              sitePref === site ? "selected=selected" : ""
-                            }value="${escapeHtml(site)}">${escapeHtml(site)}</option>`
-                        )
-                        .join("")}
-                   </select>
-                </form>
-                
-                <form action="" class="flex" style="margin-left: 5px;">
-                     <select onchange="onTimeRangeChanged()" class="range-select selector float-right shadow text-gray-800 bg-gray-400 text-sm font-bold py-2 pr-8 rounded inline-flex items-center appearance-none mr-1" name="time-range" style="color: rgba(0,0,0, 0.7); padding-left: 12px;">
-                        <option ${
-                          rangePref === "day" ? "selected=selected" : ""
-                        } value="day">Today</option>
-                        <option ${
-                          rangePref === "month" ? "selected=selected" : ""
-                        } value="month">This Month</option>
-                        <option ${
-                          rangePref === "year" ? "selected=selected" : ""
-                        } value="year">This year</option>
-                        <option ${
-                          rangePref === "all" ? "selected=selected" : ""
-                        } value="all">All</option>
+      this.innerHTML = `
+        <div class="project mr16">
+          <img src="img-delete/bell.png" width="16" height="16" alt="Favicon" id="selector-favicon">
+          <select onchange="onSiteChanged()" id="site-select">
+            ${sites
+              .map(
+                (site) =>
+                  `<option ${
+                    sitePref === site ? "selected=selected" : ""
+                  }value="${escapeHtml(site)}">${escapeHtml(site)}</option>`)
+            .join("")}
+          </select>
+        </div>
+        <select onchange="onTimeRangeChanged()" id="range-select">
+          <option ${
+            rangePref === "day" ? "selected=selected" : ""
+          } value="day">Today</option>
+          <option ${
+            rangePref === "month" ? "selected=selected" : ""
+          } value="month">This Month</option>
+          <option ${
+            rangePref === "year" ? "selected=selected" : ""
+          } value="year">This year</option>
+          <option ${
+            rangePref === "all" ? "selected=selected" : ""
+          } value="all">All</option>
+        </select>`;
 
-                     </select>
-                  </form>`;
-      document.getElementsByClassName("site-select")[0].onchange = () => {
+      let siteSelectEl = document.getElementById("site-select")
+      siteSelectEl.onchange = (evt) => {
+	this.setFavicon(evt.target.value)
         this.dispatchEvent(new Event("site-changed"));
       };
-      document.getElementsByClassName("range-select")[0].onchange = () => {
+      this.setFavicon(siteSelectEl.value)
+      document.getElementById("range-select").onchange = () => {
         this.dispatchEvent(new Event("range-changed"));
       };
       this.drawCalled = true
     }
 
+    setFavicon(domain){
+        let favicon = document.getElementById("selector-favicon")
+        favicon.src = `https://icons.duckduckgo.com/ip3/${domain}.ico`
+    }
+
     get site() {
-      return this.drawCalled && document.getElementsByClassName("site-select")[0].value;
+      return this.drawCalled && document.getElementById("site-select").value;
     }
 
     get range() {
-      return this.drawCalled &&  document.getElementsByClassName("range-select")[0].value;
+      return this.drawCalled &&  document.getElementById("range-select").value;
     }
   }
 );
