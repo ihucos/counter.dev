d0ec6b857cd9bfe85ebec9b281039ce80ce5cfd9
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2020-12-26 13:59:11 +0100

    -

--- static/comp/newdesign/pie.js
+++ static/comp/newdesign/pie.js
@@ -2,21 +2,19 @@ customElements.define(
     tagName(),
     class extends HTMLElement {
         draw(obj) {
-            if (Object.keys(obj).length > 0) {
-                this.innerHTML = `
+            this.innerHTML = `
                 <div class="metrics-headline">
                   <img src="img/browsers.svg" width="24" height="24" alt="${this.getAttribute('caption')}">
                   <h3 class="ml16">${this.getAttribute('caption')}</h3>
                 </div>
                 <div class="metrics-two-data bg-white shadow-sm radius-lg">
-                  <div style="display: flex"> <!-- another hacky container-->
-                    <comp-newdesign-piegraph  class="metrics-two-graph-wrap"></comp-newdesign-piegraph>
-                  </div>
-                  <comp-newdesign-pielegend></comp-newdesign-pielegend>
+                  ${Object.keys(obj).length > 0 ? `
+                      <div style="display: flex"> <!-- another hacky container-->
+                        <comp-newdesign-piegraph  class="metrics-two-graph-wrap"></comp-newdesign-piegraph>
+                      </div>
+                      <comp-newdesign-pielegend></comp-newdesign-pielegend>
+                      ` : `<comp-nodata></comp-nodata>`}
                 </div>`
-            } else {
-                this.innerHTML = `<comp-nodata></comp-nodata>`
-            }
         }
     }
 );
