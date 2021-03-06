63c4ad26095289dfbcaf00ccde19f2f1bb00ebdf
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2021-03-06 17:56:52 +0100

    Keep flash closed

--- static/components/counter-flash.js
+++ static/components/counter-flash.js
@@ -2,16 +2,22 @@
 customElements.define(
     tagName(),
     class extends HTMLElement {
+        KEY = 'close-enjoy-new-design-msg'
         connectedCallback() {
-            if (!window.location.href.split('#')[0].endsWith('/dashboard.html?demo=1')){
+            if ((!window.location.href.split('#')[0].endsWith('/dashboard.html?demo=1') && (!localStorage.getItem(this.KEY)))){
                 this.innerHTML += `
                    <!-- Notification -->
                    <section class="notification">
                      <div class="content">
                        <span>Enjoy our new design!</span>
-                       <div class="btn-close" onclick="this.closest('section').remove()"></div>
+                       <div class="btn-close"></div>
                      </div>
                    </section>`;
+
+                $('.btn-close').click(()=>{
+                    this.remove()
+                    localStorage.setItem(this.KEY, 'yes')
+                })
             }
         }
     }
