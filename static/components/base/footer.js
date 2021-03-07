4242ba18b8d8c9cd2e9e8a842b72c45168be300d
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2021-03-07 21:09:07 +0100

    Rework requested changes

--- static/components/base/footer.js
+++ static/components/base/footer.js
@@ -13,7 +13,7 @@ customElements.define(
                               height="32"
                               alt="Logotype"
                             />
-                            <div class="caption gray">© 2020, 2021 Counter. All rights reserved.</div>
+                            <div class="caption gray">© 2021 Counter. All rights reserved.</div>
                           </div>
                           <nav class="nav-footer">
                             <div class="nav-footer-one">
