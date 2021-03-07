c5cac8d92f7375e10fab91ea93be29d6f16d1f6e
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2021-03-07 13:57:46 +0100

    Add tracking code

--- static/track.js
+++
@@ -1,9 +0,0 @@
-if (!sessionStorage.getItem("_swa") && document.referrer.indexOf(location.protocol + "//" + location.host) !== 0) {
-    fetch("https://counter.dev/track?" + new URLSearchParams({
-        referrer: document.referrer,
-        screen: screen.width + "x" + screen.height,
-        user: "counter",
-        utcoffset: "1"
-    }))
-};
-sessionStorage.setItem("_swa", "1");
