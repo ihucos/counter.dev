50aed1170e23c3b4964dc108221621156538ef91
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2021-03-12 14:09:15 +0100

    -

--- scripts/integrations.py
+++ scripts/integrations.py
@@ -16,10 +16,13 @@ BLOCKLIST = [
 
 r = redis.StrictRedis()
 user_integrations = {}
+user_sites = {}
 
 for key in r.keys("v:*date,all"):
     vs = key.decode().split(":", 1)[-1].split(",")
     user = vs[1]
+    sites =r.hgetall("sites:" + user)
+    user_sites[user] = sites
     date_data = r.hgetall(key)
     fst_date = min(sorted(date_data.keys())).decode()
     user_integrations.setdefault(user, [])
@@ -40,9 +43,9 @@ for key in sorted(r.keys("sites:*")):
 
     integr = user_integrated_at.get(user)
     if integr:
-        data.append(dict(hits=hits, user=user, integr=integr))
+        data.append(dict(hits=hits, user=user, integr=integr, sites=user_sites[user]))
 
 
 data.sort(key=lambda i: i["integr"])
 for entry in data:
-    print("{user:<12} {integr} {hits:<6}".format(**entry))
+    print("{user:<12} {integr} {hits:<6} {sites}".format(**entry))
