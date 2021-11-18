74b81b854ea4c0bd9832f5f2d185bb64ed78b6c9
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2021-11-18 23:40:25 +0100

    recover endpoint

---
+++ backend/endpoints/recover.go
@@ -0,0 +1,19 @@
+package endpoints
+
+import "github.com/ihucos/counter.dev/lib"
+
+func init() {
+	lib.Endpoint(lib.EndpointName(), func(ctx *lib.Ctx) {
+		userId := ctx.R.FormValue("user")
+		mail := ctx.R.FormValue("mail")
+		if userId == "" {
+			ctx.ReturnBadRequest("Missing Input: user")
+		}
+		if mail == "" {
+			ctx.ReturnBadRequest("Missing Input: Email")
+		}
+
+		//user := ctx.User(userId)
+
+	})
+}
