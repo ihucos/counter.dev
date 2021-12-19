6acb5d0c5b0633267e1910fadd8276bcc25091fb
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2021-12-19 02:43:51 +0100

    Blog progressed

--- .pelican/pelicanconf.py
+++ .pelican/pelicanconf.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Irae Hueck Costa'
-SITENAME = 'Counter blog'
-SITEURL = '/blog'
+AUTHOR = "Irae Hueck Costa"
+SITENAME = "Counter blog"
+SITEURL = "/blog"
 
-THEME = 'counter'
+THEME = "counter"
 
-PATH = 'content'
+PATH = "content"
 
-OUTPUT_PATH = '../static/blog'
+OUTPUT_PATH = "../static/blog"
 
-TIMEZONE = 'Europe/Berlin'
+TIMEZONE = "Europe/Berlin"
 
-DEFAULT_LANG = 'en'
+DEFAULT_LANG = "en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -24,16 +24,39 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ("Pelican", "http://getpelican.com/"),
+    ("Python.org", "http://python.org/"),
+    ("Jinja2", "http://jinja.pocoo.org/"),
+    ("You can modify those links in your config file", "#"),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+# RELATIVE_URLS = True
+
+
+import re
+import unidecode
+
+
+def slugify(text):
+    text = str(text)
+    text = unidecode.unidecode(text).lower()
+    return re.sub(r"[\W_]+", "-", text)
+
+def readtime(text):
+    WPM = 180
+    text = str(text)
+    words = len(text.replace('\n', ' ').split())
+    return round(words / WPM) or 1
+
+
+JINJA_FILTERS = {"slugify": slugify, "readtime": readtime}
