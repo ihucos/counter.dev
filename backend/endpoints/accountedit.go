16ea13e39d3e7a4cb20bf380452e9327ea90843c
hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
2022-02-27 16:48:11 -0800

    Fix accountedit typo

--- backend/endpoints/accountedit.go
+++ backend/endpoints/accountedit.go
@@ -43,7 +43,7 @@ func init() {
 			}
 
 			if len(newPassword) < 8 {
-				ctx.ReturnBadRequest("New password must have at least 8 charachters")
+				ctx.ReturnBadRequest("New password must have at least 8 characters")
 			}
 
 			if newPassword != repeatNewPassword {
