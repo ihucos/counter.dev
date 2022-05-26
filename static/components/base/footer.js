565f30b790a3e994a505286c2bb5a5bd7b842f74
slomchinskiy <49724943+slomchinskiy@users.noreply.github.com>
2022-05-26 12:15:19 +0300

    Status link changed

--- static/components/base/footer.js
+++ static/components/base/footer.js
@@ -27,7 +27,7 @@ customElements.define(
                               <ul>
                                 <li><a href="/pages/privacy.html" rel="nofollow">Privacy</a></li>
                                 <li><a href="/pages/imprint.html" rel="nofollow">Imprint</a></li>
-                                <li><a href="https://app.pulsetic.com/status/sxRCdxOy" target="_blank" rel="nofollow" class="footer-status">Status</a></li>
+                                <li><a href="https://status.counter.dev" target="_blank" rel="nofollow" class="footer-status">Status</a></li>
                               </ul>
                             </div>
                           </nav>
