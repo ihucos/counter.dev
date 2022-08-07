0aec8571d57473fbfbb7387e9af1e3bd346a4d1a
Irae Hueck Costa <irae.hueck.costa@gmail.com>
2022-08-07 22:02:16 +0200

    Group google searches

--- static/components/dashboard/sources-countries.js
+++ static/components/dashboard/sources-countries.js
@@ -3,7 +3,17 @@ customElements.define(
     class extends HTMLElement {
         MAX_ENTRIES = 10;
 
-        drawItemSources(domain, count, totalCount) {
+        // Taken from https://www.google.com/supported_domains August 2022
+        GOOGLE_DOMAINS = ["google.com", "google.ad", "google.ae", "google.com.af", "google.com.ag", "google.com.ai", "google.al", "google.am", "google.co.ao", "google.com.ar", "google.as", "google.at", "google.com.au", "google.az", "google.ba", "google.com.bd", "google.be", "google.bf", "google.bg", "google.com.bh", "google.bi", "google.bj", "google.com.bn", "google.com.bo", "google.com.br", "google.bs", "google.bt", "google.co.bw", "google.by", "google.com.bz", "google.ca", "google.cd", "google.cf", "google.cg", "google.ch", "google.ci", "google.co.ck", "google.cl", "google.cm", "google.cn", "google.com.co", "google.co.cr", "google.com.cu", "google.cv", "google.com.cy", "google.cz", "google.de", "google.dj", "google.dk", "google.dm", "google.com.do", "google.dz", "google.com.ec", "google.ee", "google.com.eg", "google.es", "google.com.et", "google.fi", "google.com.fj", "google.fm", "google.fr", "google.ga", "google.ge", "google.gg", "google.com.gh", "google.com.gi", "google.gl", "google.gm", "google.gr", "google.com.gt", "google.gy", "google.com.hk", "google.hn", "google.hr", "google.ht", "google.hu", "google.co.id", "google.ie", "google.co.il", "google.im", "google.co.in", "google.iq", "google.is", "google.it", "google.je", "google.com.jm", "google.jo", "google.co.jp", "google.co.ke", "google.com.kh", "google.ki", "google.kg", "google.co.kr", "google.com.kw", "google.kz", "google.la", "google.com.lb", "google.li", "google.lk", "google.co.ls", "google.lt", "google.lu", "google.lv", "google.com.ly", "google.co.ma", "google.md", "google.me", "google.mg", "google.mk", "google.ml", "google.com.mm", "google.mn", "google.ms", "google.com.mt", "google.mu", "google.mv", "google.mw", "google.com.mx", "google.com.my", "google.co.mz", "google.com.na", "google.com.ng", "google.com.ni", "google.ne", "google.nl", "google.no", "google.com.np", "google.nr", "google.nu", "google.co.nz", "google.com.om", "google.com.pa", "google.com.pe", "google.com.pg", "google.com.ph", "google.com.pk", "google.pl", "google.pn", "google.com.pr", "google.ps", "google.pt", "google.com.py", "google.com.qa", "google.ro", "google.ru", "google.rw", "google.com.sa", "google.com.sb", "google.sc", "google.se", "google.com.sg", "google.sh", "google.si", "google.sk", "google.com.sl", "google.sn", "google.so", "google.sm", "google.sr", "google.st", "google.com.sv", "google.td", "google.tg", "google.co.th", "google.com.tj", "google.tl", "google.tm", "google.tn", "google.to", "google.com.tr", "google.tt", "google.com.tw", "google.co.tz", "google.com.ua", "google.co.ug", "google.co.uk", "google.com.uy", "google.co.uz", "google.com.vc", "google.co.ve", "google.vg", "google.co.vi", "google.com.vn", "google.vu", "google.ws", "google.rs", "google.co.za", "google.co.zm", "google.co.zw", "google.cat"]
+
+        drawItemSources(ref, count, totalCount) {
+            let item = this.groupItems[ref]
+
+            // hack
+            if (item === undefined){
+                return ''
+            }
+
             return `
           <div class="sources-countries-item shadow-sm mb8">
             <div class="percent-line" style="width: ${percentRepr(
@@ -14,15 +24,15 @@ customElements.define(
               <span>
                 <img
                   src="https://icons.duckduckgo.com/ip3/${escapeHtml(
-                      domain
+                      item.icon
                   )}.ico"
                   width="16"
                   height="16"
-                  alt="${escapeHtml(domain)}">
+                  alt="${escapeHtml(item.group)}">
                 <a href="//${escapeHtml(
-                    domain
+                    item.link
                 )}" class="black" target="_blank" rel="nofollow">
-                ${escapeHtml(domain)}
+                ${escapeHtml(item.group)}
                 </a>
               </span>
               <span>
@@ -60,11 +70,24 @@ customElements.define(
           </div>`;
         }
 
-        refGroup(ref) {
+        groupItem(ref) {
             if (ref.startsWith("www.")) {
                 ref = ref.slice(4);
             }
-            return ref;
+            let item = {
+                group: ref,
+                link: ref,
+                icon: ref
+             }
+
+            if (this.GOOGLE_DOMAINS.includes(ref)){
+                item = {
+                    group: "Google search",
+                    link: "google.com",
+                    icon: "google.com"
+                 }
+            }
+            return item;
         }
 
         draw(sources, countries) {
@@ -73,13 +96,17 @@ customElements.define(
             // Group similar looking sources (e.G. www.example.com and
             // example.com)
             let parentThis = this;
-            let groupedSources = {};
+            let groupedSources = {link: null, icon: null};
+            let groupItems = {}
             Object.keys(sources).forEach(function (ref) {
                 let refVisits = sources[ref];
-                let refGroup = parentThis.refGroup(ref);
-                groupedSources[refGroup] = groupedSources[refGroup] || 0;
-                groupedSources[refGroup] += refVisits;
+                let groupItem = parentThis.groupItem(ref);
+                groupItems[groupItem.group] = groupItem
+                groupedSources[groupItem.group] = groupedSources[groupItem.group] || 0;
+                groupedSources[groupItem.group] += refVisits;
             });
+
+            this.groupItems = groupItems
 
             this.allSourcesEntries = Object.entries(groupedSources).sort(
                 (a, b) => b[1] - a[1]
