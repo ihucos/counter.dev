36083310504271949c2b4ec71c9541e259dd8146
Irae Hueck Costa <mail@irae.me>
2022-09-17 18:49:46 +0000

    Updated tracking test cdn tracking script

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,21 @@
-if (!sessionStorage.getItem("_swa") && document.referrer.indexOf(location.protocol + "//" + location.host) !== 0) {
-    fetch("https://counter.dev/track?" + new URLSearchParams({
-        referrer: document.referrer,
-        screen: screen.width + "x" + screen.height,
-        id: "6f0d1527-8dfa-4ead-9e9d-1ab2334c391d",
-        utcoffset: "2"
+// UNDER DEVELOPMENT DO NOT USE
+(function() {
+
+    var id = document.currentScript.getAttribute("data-id")
+    var utcoffset = document.currentScript.getAttribute("data-utcoffset")
+    var server = document.currentScript.getAttribute("data-server") || "https://counter.dev/"
+
+    if (!sessionStorage.getItem("_swa") && !document.referrer.startsWith(location.protocol + "//" + location.host)) {
+        fetch(server + "/track?" + new URLSearchParams({
+            referrer: document.referrer,
+            screen: screen.width + "x" + screen.height,
+            id: id,
+            utcoffset: utcoffset,
+        }))
+    };
+    sessionStorage.setItem("_swa", "1");
+    navigator.sendBeacon(server + "/trackpage", new URLSearchParams({
+        id: document.currentScript.getAttribute("data-id"),
+        page: window.location.pathname
     }))
-};
-sessionStorage.setItem("_swa", "1");
+})()
