753b2b86088fe4dd25cd19552c9f0e78640f1833
Irae Hueck Costa <mail@irae.me>
2022-09-24 15:00:48 +0000

    Show platforms in visit

--- backend/endpoints/track.go
+++ backend/endpoints/track.go
@@ -149,7 +149,7 @@ func init() {
 		//
 		// save visit map
 		//
-		logLine := fmt.Sprintf("[%s] %s %s %s", now.Format("2006-01-02 15:04:05"), country, refParam, device)
+		logLine := fmt.Sprintf("[%s] %s %s %s %s", now.Format("2006-01-02 15:04:05"), country, refParam, device, platform)
 
 		siteId := Origin2SiteId(origin)
 
